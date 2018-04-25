248c6c95ea2a29f2d71a65014a01569e8b73b26b
IntoThePit <theblindorphan@gmail.com>
2018-04-25 16:35:27 +0100

    The herring now go on a quest into the sky! We have movement!

--- @herring/migrate.m
+++ @herring/migrate.m
@@ -61,6 +61,8 @@ sep_weight = 0.6;
 align_weight = 0.25;
 cohes_weight = 0.15;
 overall_force = (sep_weight * tot_sep_force) + (align_weight * tot_align_force) + (cohes_weight * tot_cohes_force);
+movement_vect = steer(agt.vel, overall_force);
+agt.pos = agt.pos + movement_vect
 % TODO(Pierre): Some function that Steers based on force. Like it
 % shouldn't turn too fast or go faster than herring can go.
 % Desired velocity, current velocity
