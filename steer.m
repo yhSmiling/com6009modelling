34b36109c7faceac69efdd0814a2723375df6474
IntoThePit <theblindorphan@gmail.com>
2018-04-25 16:25:58 +0100

    Implemented steering function which will not go faster than the max speed or turn quicker than the max angle
    Hopefully this will be robust against any sorts of crazy inputs, so we don't need to worry.

--- steer.m
+++ steer.m
@@ -4,15 +4,31 @@ function [ movement_vect ] = steer( desired_vect, curr_vect )
 %   Detailed explanation goes here
 
 % Probably should get this from the agent
-max_turning_rate = 30;
+max_turning_rate = 10; % In degrees per iteration
 max_accel = 7;
 max_speed = 15;
 
 curr_speed = norm(curr_vect);
 desired_speed = norm(desired_vect);
 
-% This gives the angle between the two vectors in degrees.
+% This gives the angle between the two vectors in degrees. Value between 0
+% and 180
 angle_diff = acosd(dot(curr_vect, desired_vect) / (curr_speed * desired_speed));
+if angle_diff > max_turning_rate
+    
+    current_angle = atand(curr_vect(2)/curr_vect(1));
+    desired_angle = atand(desired_vect(2)/desired_vect(1));
+    
+    angle_diff = max_turning_rate;
+    % TODO(Pierre): How do you tell whether to add or subtract angle_diff?
+    if desired_angle < current_angle
+        new_angle = current_angle - angle_diff;
+    else
+        new_angle = current_angle + angle_diff;
+    end
+    
+    desired_vect = [(desired_speed * cosd(new_angle)), (desired_speed * sind(new_angle))];
+end
 
 if desired_speed > 0
     unit_vect = desired_vect / desired_speed;
@@ -27,16 +43,13 @@ end
 % NOTE(Pierre): Not really sure about this. Idea is sometimes speed diff is
 % pos, sometimes it's neg. If it's big, you want to accelerate a lot (but
 % not more than max_accel)
-speed_diff = (desired_speed-current_speed)/max_speed;
+speed_diff = (desired_speed-curr_speed)/max_speed;
 final_accel = speed_diff * max_accel;
 
 output_speed = curr_speed + final_accel;
 
-output_vect = output_speed * unit_vect;
+movement_vect = output_speed * unit_vect;
 
-desired_speed
-unit_vect
-output_vect
 
 
 end
